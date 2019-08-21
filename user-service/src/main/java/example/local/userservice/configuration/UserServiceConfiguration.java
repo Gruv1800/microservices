715a75a493501132e8f4fc52102d28c84217eac2
gruv1800 <D@cbkbq180090>
2019-08-21 15:51:09 +0300

    User service implementation.

---
+++ user-service/src/main/java/example/local/userservice/configuration/UserServiceConfiguration.java
@@ -0,0 +1,7 @@
+package example.local.userservice.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserServiceConfiguration {
+}
---
+++ user-service/src/main/java/example/local/userservice/model/UserEntity.java
@@ -0,0 +1,33 @@
+package example.local.userservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@Entity
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Email
+    @NotNull
+    @NotBlank
+    private String email;
+    @NotNull
+    @NotBlank
+    private String login;
+    @NotNull
+    @NotBlank
+    private String password;
+
+}
